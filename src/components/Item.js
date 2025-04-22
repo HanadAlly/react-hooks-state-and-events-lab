2485a854573a55d0ca90f5930004acc0483f0cdf
HanadAlly <allyhanad404@gmail.com>
2025-04-22 15:07:05 +0300

    commit changes

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,21 @@
-import React from "react";
+ import React, { useState } from "react";
 
-function Item({ name, category }) {
-  return (
-    <li className="">
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
-    </li>
-  );
-}
+ function Item({ name, category }) {
+   const [isInCart, setIsInCart] = useState(false);
 
-export default Item;
+   const handleClick = () => {
+     setIsInCart((prevMode) => !prevMode);
+   };
+
+   return (
+     <li className={isInCart ? "in-cart" : ""}>
+       <span>{name}</span>
+       <span className="category">{category}</span>
+       <button className={isInCart ? "remove" : "add"} onClick={handleClick}>
+         Add to Cart
+       </button>
+     </li>
+   );
+ }
+
+ export default Item;
